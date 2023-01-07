5000151e3d9a1a414afde0b22e9fe4a0d3b3ead5
boriz81 <blitzboriz@gmail.com>
2023-01-07 14:35:45 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = 0;
+        while (scanner.nextInt() > 0) {
+                ++n;
+            } System.out.println(n);
+        }
+}
